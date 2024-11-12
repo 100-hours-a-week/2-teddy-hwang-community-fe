68bce70b65b1e483dc9f00d7bab19a2f315c24d0
ghkddlscks19 <ghkddlscks19@naver.com>
2024-11-12 10:35:15 +0900

    feat: 게시글 작성 버튼 함수틀 작성

---
+++ js/board.js
@@ -0,0 +1,10 @@
+/**
+ * 게시글 작성 버튼을 누를 시 게시글 작성 페이지로 이동
+ * 호버 시 ACA0EB 색상에서 7F6AEE 색상으로 변경
+ * 언호버 시 7F6AEE 색상에서 ACA0EB 색상으로 변경
+ */
+const createPostBtn = function(){
+    const createPostBtn = document.getElementById('create-board-btn');
+    
+
+};
