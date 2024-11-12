56d1e2df93d9051cedcbb13cd40f6bcb7447b9d3
ghkddlscks19 <ghkddlscks19@naver.com>
2024-11-12 11:57:42 +0900

    feat: 제목 26자 길이 제한 및 엔터키 금지

--- js/board-create.js
+++ js/board-create.js
@@ -22,6 +22,27 @@ const dropdown = function(){
         dropdown.classList.remove('show');   
     }); 
 }
+/**
+ * 제목은 26자까지 작성 가능
+ * 27자 이상 작성시 작성 안됨
+ */
+const titleMaxLength = function(input, maxLength){
+    if(input.value.length > maxLength){
+        input.value = input.value.slice(0, maxLength);
+    }
+}
+
+//title enter키 입력 방지
+const titleBanEnter = function(){
+    const title = document.getElementById('title');
+    //enter키 막기
+    title.addEventListener('keydown', (event) => {
+        if(event.code === 'Enter') event.preventDefault();
+    });
+    title.addEventListener()
+}
+
 
 back();
 dropdown();
+titleBanEnter();
