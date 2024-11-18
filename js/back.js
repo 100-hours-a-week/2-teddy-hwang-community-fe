4369383cc18084b054ebde82be174ee651774de4
ghkddlscks19 <ghkddlscks19@naver.com>
2024-11-18 11:20:20 +0900

    feat: 뒤로가기 기능

---
+++ js/back.js
@@ -0,0 +1,8 @@
+//뒤로가기
+const back = function(){
+    const backIcon = document.querySelector('.material-icons');
+
+    backIcon.addEventListener('click', () => {
+        history.back();
+    });
+}
