3a1f9fe19946ace78691467107d58400ff4779b2
ghkddlscks19 <ghkddlscks19@naver.com>
2024-11-18 16:53:53 +0900

    feat: 글 및 유저정보 불러오기

--- js/user-modify.js
+++ js/user-modify.js
@@ -44,13 +44,78 @@ const userModal = function() {
     });
     //확인
     userCheckBtn.addEventListener('click', () => {
-        
+        //탈퇴 처리
+
+        //로그인으로 이동
+        location.href = '../html/login.html';
     });
 }
 
-const modalContainer = document.querySelector('.modal-container');
+/**
+ * 수정하기 버튼 클릭했을 때
+ * 닉네임 입력X -> *닉네임을 입력해주세요.
+ * 닉네임 중복시 -> *중복된 닉네임 입니다.
+ * 닉네임 11자 이상 작성시 -> *닉네임은 최대 10자 까지 작성 가능합니다.
+ */
+const nicknameInput = function(){
+    const nicknameInput = document.getElementById('nickname');
+    const modifyBtn = document.getElementById('modify-btn');
+    const helpertext = document.getElementById('helpertext');
+
+    const showNicknameError = function(nickname){
+
+        //닉네임을 입력하지 않은 경우
+        if(nickname === ''){
+            helpertext.textContent = '*닉네임을 입력해주세요.';
+            nicknameValid = false;
+            return;
+        }
+        if(!nicknameIsValid(nickname)){       
+            //닉네임에 띄어쓰기가 포함된 경우
+            if(nickname.includes(' ')){
+                helpertext.textContent = '*띄어쓰기를 없애주세요.';
+                nicknameValid = false;
+                return;
+            }
+            //닉네임 길이가 10글자 초과하는 경우
+            if(nickname.length > 10){
+                helpertext.textContent = '*닉네임은 최대 10자 까지 작성 가능합니다.';
+                nicknameValid = false;
+                return;
+            }
+            //닉네임 중복시 추가 작성해야함
+        }
+        helpertext.textContent = '';
+        nicknameValid = true;
+        return;
+    }
+
+    modifyBtn.addEventListener('click', () => {
+        showNicknameError(nicknameInput.value);
+        if(nicknameValid) toastMessage();
+    });
+
+};
+
+const nicknameIsValid = (nickname) => {
+    //띄어쓰기 없이 최대 10글자
+    const nicknamePattern = /^[^\s]{1,10}$/; 
+    return nicknamePattern.test(nickname);
+};
+//토스트 메시지 1초 보여주기
+const toastMessage = function(){
+    const toast = document.getElementById('toast');
+    toast.classList.add('show');
+    setTimeout(() => {
+        toast.classList.remove('show');
+    }, 1000);
+}
+
+let nicknameValid;
+const modalContainer = document.getElementById('modal-container');
 const body = document.body;
 
-dropdown();
+
 userDeleteText();
 userModal();
+nicknameInput();
