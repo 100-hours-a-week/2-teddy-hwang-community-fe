22b501be47b0c3815d432dec08a156bdaf02b223
ghkddlscks19 <ghkddlscks19@naver.com>
2024-12-03 10:19:19 +0900

    fix: referer -> history.back()으로 수정

--- js/back.js
+++ js/back.js
@@ -6,7 +6,11 @@ const back = () => {
   const backIcon = document.querySelector(".material-icons");
 
   backIcon.addEventListener("click", () => {
-    location.href = document.referrer;
+      if (window.history.length > 1) {
+        window.history.back();
+      } else {
+        window.location.href = '/'; // 메인 페이지 URL로 변경하세요
+      }
   });
 };
 
